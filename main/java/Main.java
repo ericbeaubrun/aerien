9a6186a8ebbec7f365843a69fc4ba9141e44ce62
Eric <154993587+ericbeaubrun@users.noreply.github.com>
2024-01-13 18:55:59 +0100

    premier programme

--- main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import config.Config;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Config.BLOCK_SIZE);
-        MainWindow frame = new MainWindow();
-    }
-}
